f3ce7ed966e2765211588bbbbfb6073c4a3f0937
Mau2g <M4u2g0@gmail.com>
2025-08-22 09:48:06 -0500

    Crear makebombzip.py

---
+++ makebombzip.py
@@ -0,0 +1,13 @@
+import os
+
+def crear_archivo(nombre, tamaño_mb):
+    
+    tamaño_bytes = tamaño_mb * 1024 * 1024  
+    
+    with open(nombre, "wb") as f:
+ 
+        f.write(b"\0" * tamaño_bytes)
+
+    print(f"Archivo '{nombre}' creado con {tamaño_mb} MB.")
+
+crear_archivo("archivo_100mb.bin") 
