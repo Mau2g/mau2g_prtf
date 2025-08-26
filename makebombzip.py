9e6c6be4a2706ca95dce2f8ff81647b965e4a50a
mau_2g <M4u2g0@gmail.com>
2025-08-26 09:38:16 -0500

    Aleatorizacion de datos

--- makebombzip.py
+++ makebombzip.py
@@ -1,13 +1,15 @@
 import os
+import string
 
 def crear_archivo(nombre, tamaño_mb):
-    
-    tamaño_bytes = tamaño_mb * 1024 * 1024  
-    
+    tamaño_bytes = tamaño_mb * 1024 * 1024
+    alfanum = (string.ascii_letters + string.digits).encode('ascii')
+    n = len(alfanum)
+
     with open(nombre, "wb") as f:
- 
-        f.write(b"\0" * tamaño_bytes)
+        for _ in range(tamaño_bytes):
+            f.write(alfanum[os.urandom(1)[0] % n:n % n + 1])
 
-    print(f"Archivo '{nombre}' creado con {tamaño_mb} MB.")
+    print(f"Archivo '{nombre}' creado con {tamaño_mb} MB de caracteres alfanuméricos.")
 
-crear_archivo("archivo_100mb.bin",100) 
+crear_archivo("archivo_100mb.bin", 100)
